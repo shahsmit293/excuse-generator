97b3ec420cc6e1f8238579316059ac146859b607
shahsmit293 <138520584+shahsmit293@users.noreply.github.com>
2023-07-10 00:57:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function Excuse() {
 	
-	var live = ['ninja', 'cat', 'grandma','owner'];
+	var person = ['father', 'mother', 'grandma','grandfather'];
 	var action = ['kicked', 'snapped', 'minced','throw'];
 	var work = ['homework', 'project', 'report','presentation'];
+	var time=['on monday','on thursday','onf riday','on sunday']
 	
-	var who = live[Math.round(Math.random()*(live.length-1))];
+	var who = person[Math.round(Math.random()*(person.length-1))];
 	var did = action[Math.round(Math.random()*(action.length-1))];
 	var what = work[Math.round(Math.random()*(work.length-1))];
-	
-	document.getElementById('excuses').innerHTML = '<div>My ' + who + ' ' + did + ' my ' + what + '.</div>'
+	var when = time[Math.round(Math.random()*(time.length-1))];
+	document.getElementById('excuses').innerHTML = '<div>My ' + who + ' ' + did + ' my ' + what +' '+ when + '.</div>'
 	
 }
